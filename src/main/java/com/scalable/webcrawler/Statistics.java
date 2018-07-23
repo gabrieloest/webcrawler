192c60ff6846d7727aa6d9daa7fe3177ea8e0d6e
Gabriel Oest <gabriel.oest@esig.com.br>
2018-07-23 16:58:08 -0300

    Refactory statistics

--- src/main/java/com/scalable/webcrawler/Statistics.java
+++ src/main/java/com/scalable/webcrawler/Statistics.java
@@ -7,11 +7,19 @@ import java.util.stream.Collectors;
 
 public class Statistics {
 
-	public static void countLibraries(List<String> listOfLibraries) {
-		Map<String, Long> counted = listOfLibraries.stream()
-				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	public void countLibraries(List<String> listOfLibraries) {
+		Map<String, Long> counted = GroupingAndCountLibraries(listOfLibraries);
 
+		printTop5UsedLibraries(counted);
+	}
+
+	private void printTop5UsedLibraries(Map<String, Long> counted) {
 		counted.entrySet().stream().sorted((c1, c2) -> c2.getValue().compareTo(c1.getValue())).limit(5)
 				.forEach(System.out::println);
+	}
+
+	private Map<String, Long> GroupingAndCountLibraries(List<String> listOfLibraries) {
+		return listOfLibraries.stream()
+				.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 	}
 }
